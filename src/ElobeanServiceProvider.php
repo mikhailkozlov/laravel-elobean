a9d97004409259a087592d73b6bf543856fac29f
Mikhail Kozlov <code@315design.com>
2017-09-14 17:58:20 -0400

    WIP

---
+++ src/ElobeanServiceProvider.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace MikhailKozlov\Elobean;
+
+use Illuminate\Support\ServiceProvider;
+
+class ElobeanServiceProvider extends ServiceProvider
+{
+    /**
+     * Boot the service provider.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+    }
+}
